2bbb4bbb4c9f82ec89a100f76073cdcbc326bd71
Wiiplay123 <TheRealWiiplay123@gmail.com>
2016-04-02 11:22:52 -0500

    Updated version values because I forgot to do it earlier
    Sorry :c

--- robin-assistant.user.js
+++ robin-assistant.user.js
@@ -4,7 +4,7 @@
 // @namespace   com.github.leoverto
 // @include     https://www.reddit.com/robin/
 // @include     https://www.reddit.com/robin
-// @version     1.6
+// @version     1.7
 // @author      LeoVerto, Wiiplay123, Getnamo
 // @grant       none
 // ==/UserScript==
@@ -12,7 +12,7 @@
 var autoVote = true;
 var disableVoteMsgs = true;
 var filterSpam = true;
-var version = "1.6";
+var version = "1.7";
 
 var ownName = $('.user a').text();
 var filteredSpamCount = 0;
